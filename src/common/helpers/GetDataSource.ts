5a90ca5f2fc9b04e963a0a454962900e9d48e86d
GuilhermeHenrique659 <guilhermehenrique.o@hotmail.com>
2023-10-03 14:27:44 -0300

    fix build

--- src/common/helpers/GetDataSource.ts
+++ src/common/helpers/GetDataSource.ts
@@ -1,9 +1,10 @@
+import { Type } from "@common/types/DecoractorType";
 import DataSource from "../database/datasource/DataSource";
 import QueryBuilder from "../database/datasource/QueryBuilder";
 import IEntity from "../database/datasource/types/IEntity";
 import { Transaction } from "neo4j-driver";
 
-const GetDatasource = <T extends IEntity>(entity: new (...agrs: any[]) => IEntity, tx: Transaction) => {
+const GetDatasource = <T extends IEntity>(entity: Type<T>, tx: Transaction) => {
     return new DataSource<T>(new QueryBuilder(tx), entity);
 }
 
